861cd5c4027f0a3fa53534d322a22a5e48e2f3e7
Alex Dunn <dunn.alex@gmail.com>
2016-04-04 07:10:19 -0700

    roswell 0.0.4.57 (new formula)

---
+++ Formula/roswell.rb
@@ -0,0 +1,28 @@
+class Roswell < Formula
+  desc "Lisp installer and launcher for major environments"
+  homepage "https://github.com/roswell/roswell"
+  url "https://github.com/roswell/roswell/archive/v0.0.4.57.tar.gz"
+  sha256 "05985f10c8577bba664f6c5db3baa60d41704d76702609107599689504a93fd0"
+  head "https://github.com/roswell/roswell.git"
+
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+
+  def install
+    system "./bootstrap"
+    system "./configure", "--bindir=#{libexec}",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--enable-manual-generation",
+                          "--enable-html-generation",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    (bin/"ros").write_env_script(libexec/"ros", :ROSWELL_HOME => var/"roswell")
+  end
+
+  test do
+    ENV["ROSWELL_HOME"] = testpath
+    system libexec/"ros", "init"
+    File.exist? testpath/".roswell/config"
+  end
+end
