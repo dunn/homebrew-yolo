46e7f9cdf2f56c20f6d5acc751c88672b36b338c
Alex Dunn <dunn.alex@gmail.com>
2017-01-17 18:13:45 -0800

    cmucl 21b, 2017-01 (devel)

--- Formula/cmucl.rb
+++ Formula/cmucl.rb
@@ -1,18 +1,18 @@
 class Cmucl < Formula
   desc "High-performance, free Common Lisp implementation"
   homepage "https://www.cons.org/cmucl/"
-  url "https://common-lisp.net/project/cmucl/downloads/release/21a/cmucl-src-21a.tar.bz2"
-  sha256 "41604a4f828a134dbf8a58623f45bd81b76ae05fc5c4cea5ccb74edfdc9e3167"
+  url "https://common-lisp.net/project/cmucl/downloads/release/21b/cmucl-src-21b.tar.bz2"
+  sha256 "6f9ea5920d38d6881cee85d976565b3a6070bf6473dd64254e0e9f2f23625875"
   head "https://gitlab.common-lisp.net/cmucl/cmucl.git"
 
   resource "bootstrap" do
-    url "https://common-lisp.net/project/cmucl/downloads/snapshots/2016/06/cmucl-2016-06-x86-darwin.tar.bz2"
-    sha256 "6a326515b2015f5dcb8f7f6517cecbf33fdf1bf375adb13c281dea7794d607e3"
+    url "https://common-lisp.net/project/cmucl/downloads/snapshots/2017/01/cmucl-2017-01-x86-darwin.tar.bz2"
+    sha256 "a458035577a7c5893f36da457087679999a3e472b0ad0fd3ad122a35630061a7"
   end
 
   devel do
-    url "https://common-lisp.net/project/cmucl/downloads/snapshots/2016/06/cmucl-src-2016-06.tar.bz2"
-    sha256 "14fdfb571123df1d02520a97c7beecd253cc0c7f0ecbe9714b9014b216f20735"
+    url "https://common-lisp.net/project/cmucl/downloads/snapshots/2017/01/cmucl-src-2017-01.tar.bz2"
+    sha256 "1b6b813eca3d48652ca69529ffc4819598e3d0e21ac76f74c66a928bd7bfd062"
   end
 
   def install
