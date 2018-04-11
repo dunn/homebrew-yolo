25bd5e12d0884d7d1bd178237c07390bb53f3408
Alex Dunn <dunn.alex@gmail.com>
2018-04-11 09:16:46 -0700

    openrefine 2.8

--- Formula/openrefine.rb
+++ Formula/openrefine.rb
@@ -1,30 +1,23 @@
 class Openrefine < Formula
   desc "Tools for working with messy data"
   homepage "http://openrefine.org"
-  url "https://github.com/OpenRefine/OpenRefine/archive/2.5.tar.gz"
-  sha256 "bd1692093e9a34393fad6e06fc2113974f58d37b501f2542de934223692887a4"
-
-  devel do
-    url "https://github.com/OpenRefine/OpenRefine/archive/2.6-rc.2.tar.gz"
-    version "2.6-rc.2"
-    sha256 "0b790d0c43a88004874f1cfa298ad66388443b6ddb9392cc628fff84ee1a79cb"
-  end
+  url "https://github.com/OpenRefine/OpenRefine/archive/2.8.tar.gz"
+  sha256 "a7e00b404d7d11f2da7f01977e4e920c23e227c7f025cc2593c17a088d62da33"
 
   def install
-    ENV.java_cache
     system "./refine", "build"
     libexec.install Dir["*"]
     bin.write_exec_script libexec/"refine", libexec/"unsign"
   end
 
   test do
-    ENV.java_cache
     begin
       pid = fork do
         exec bin/"refine"
       end
       sleep 5
       system "curl", "127.0.0.1:3333"
+      sleep 5
     ensure
       Process.kill "TERM", pid
       Process.wait pid
